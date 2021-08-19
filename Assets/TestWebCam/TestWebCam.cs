b4f04803134285926b9635bc0517d05696d9a08f
guoliulong <guoliulong@gmail.com>
2021-08-19 22:54:07 +0800

    第一次

---
+++ Assets/TestWebCam/TestWebCam.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class TestWebCam : MonoBehaviour
+{
+    WebCamTexture webCamTex;
+    CommandBuffer cmd;
+    RenderTexture tempTex;
+    IEnumerator Start()
+    {
+        webCamTex = new WebCamTexture(WebCamTexture.devices[1].name,Screen.width, Screen.height);
+        webCamTex.Play();
+        webCamTex.wrapMode = TextureWrapMode.Repeat;
+        tempTex = new RenderTexture(Screen.width, Screen.height,0);
+        tempTex.wrapMode = TextureWrapMode.Repeat;
+        cmd = new CommandBuffer();
+        cmd.name = "WebCam";
+        GetComponent<Camera>().AddCommandBuffer(CameraEvent.BeforeForwardOpaque, cmd);
+        yield return new WaitForSeconds(1);
+    }
+    private void OnGUI()
+    {
+        GUILayout.BeginVertical();
+        if (webCamTex)
+        {
+            GUILayout.Label($"videoVerticallyMirrored={webCamTex.videoVerticallyMirrored}");
+            GUILayout.Label($"videoRotationAngle={webCamTex.videoRotationAngle}");
+        }
+        GUILayout.EndVertical();
+    }
+    void Update()
+    {
+       if(webCamTex)
+        {
+            cmd.Clear();
+            cmd.Blit(webCamTex, tempTex);
+
+            Vector2 rotate = Vector2.one;
+            if(webCamTex.videoRotationAngle == 180)
+            {
+                rotate = new Vector2(-1, -1);
+            }
+            cmd.Blit(tempTex, BuiltinRenderTextureType.None, new Vector2(-1, webCamTex.videoVerticallyMirrored ? -1 : 1) * rotate, Vector2.zero);
+        }
+    }
+}
---
+++ Assets/TestWebCam/TestWebCam2.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestWebCam2 : MonoBehaviour
+{
+    public RawImage rawImage;
+    public Button changeCameraBtn;
+
+    private bool mIsFront;
+
+    private WebCamTexture camTexture;
+
+    private void Start()
+    {
+        changeCameraBtn.onClick.AddListener(() => OpenCamera(mIsFront));
+    }
+
+    void OpenCamera(bool isFront)
+    {
+        mIsFront = !mIsFront;
+       foreach (var camd in  WebCamTexture.devices)
+        {
+            if(camd.isFrontFacing == isFront)
+            {
+                if (camTexture)
+                    DestroyImmediate(camTexture);
+                camTexture = new WebCamTexture(camd.name, Screen.width, Screen.height);
+                rawImage.texture = camTexture;
+                camTexture.Play();
+            }
+        }
+    }
+
+    private void OnGUI()
+    {
+        GUILayout.BeginVertical();
+        if(camTexture)
+        {
+            GUILayout.Label($"videoVerticallyMirrored={camTexture.videoVerticallyMirrored}");
+            GUILayout.Label($"videoRotationAngle={camTexture.videoRotationAngle}");
+
+            rawImage.transform.localRotation = Quaternion.Euler(0, 0, camTexture.videoRotationAngle);
+        }
+        GUILayout.EndVertical();
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
